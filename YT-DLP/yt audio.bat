d54ec0e6102f57179e50fc5370d45265c9d81933
khewweifeng <54496743+khewweifeng@users.noreply.github.com>
2023-03-13 23:46:10 +0800

    Update yt audio.bat

--- YT-DLP/yt audio.bat
+++ YT-DLP/yt audio.bat
@@ -2,7 +2,7 @@
 :start
 set /p y= "YT LINK: "
 
-echo yt-dlp -U --yes-playlist -f "ba" %y% | clip
+echo yt-dlp -U --yes-playlist -f "ba" -x --audio-format mp3 %y% | clip
 start /d C:\Users\kheww\videos
 goto start
 
